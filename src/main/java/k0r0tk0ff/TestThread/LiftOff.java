6e5393ee4c1e1cde11769dde3401dc0d1c9ddec3
k0r0tk0ff <korotkov34reg@gmail.com>
2016-11-07 11:03:20 +0000

    work realization of programm whith thread

--- src/main/java/k0r0tk0ff/TestThread/LiftOff.java
+++ src/main/java/k0r0tk0ff/TestThread/LiftOff.java
@@ -1,7 +1,47 @@
 package k0r0tk0ff.TestThread;
 
 /**
- * Created by root on 11/7/16.
+ * @(#)LiftOff.java
+ *
+ * Copyright (c)
+ * Free to any use.
+ *
+ * @author    Bryus Ekkel
+ * @author    k0r0tk0ff
+ * @since 		07/11/2016
+ * @version		1
+ *
+ * Countdown with use threads.
+ * (Programm test threads)
+ *
+ * Use SOLID princips.
  */
-public class LiftOff {
+public class LiftOff implements Runnable {
+    protected int countDown = 10;
+
+    /**
+     * Theese variable need to understand,
+     * that in every thread id rather
+     * (+1 in every thread)
+     */
+    private static int taskCount = 0;
+    private final int id = taskCount++;
+
+    public LiftOff(){}
+    public LiftOff(int countDown){
+        this.countDown = countDown;
+    }
+
+    public String status(){
+        return "#" + id + "#" +
+                (countDown > 0 ? countDown: "LiftOff!");
+    }
+
+    public void run(){
+        while(countDown-- > 0){
+            System.out.println(status());
+            Thread.yield();
+        }
+    }
+
 }
--- src/main/java/k0r0tk0ff/TestThread/Starter.java
+++ src/main/java/k0r0tk0ff/TestThread/Starter.java
@@ -1,9 +1,47 @@
 package k0r0tk0ff.TestThread;
 
+import k0r0tk0ff.TestThread.LiftOff;
+
+/**
+ * @(#)Starter.java
+ *
+ * Copyright (c)
+ * Free to any use.
+ *
+ * @author    Bryus Ekkel
+ * @author    k0r0tk0ff
+ * @since 		07/11/2016
+ * @version		1
+ *
+ * Start of main logic of programm.
+ * (Programm test threads)
+ *
+ * Use SOLID princips.
+ */
+
 public class Starter {
 
     public static void main(String[] arg){
 
-        System.out.println("Start success");
+        System.out.println("Start success \n");
+
+        /**
+         * Method one.
+         */
+        System.out.println("Method one \n");
+
+
+        LiftOff liftOff = new LiftOff(10);
+        liftOff.run();
+
+        /**
+         * Method two.
+         */
+
+        System.out.println("\n\n Method two \n");
+
+        Thread thread = new Thread(new LiftOff(10));
+        thread.start();
     }
 }
+
