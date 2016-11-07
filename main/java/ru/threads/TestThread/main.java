e4a95089552cb477a3aa3363809a870948627f0b
k0r0tk0ff <korotkov34reg@gmail.com>
2016-11-07 09:26:47 +0000

    add commit

---
+++ main/java/ru/threads/TestThread/main.java
@@ -0,0 +1,21 @@
+package ru.threads.TestThread;
+
+/**
+ * @(#)InteractRunner.java
+ *
+ * Copyright (c)
+ * Free to any use.
+ *
+ * @author    k0r0tk0ff
+ * @since 		07/11/2016
+ * @version		1.0
+ *
+ *
+ * Use SOLID princips.
+ */
+class Main{
+
+public void main(String[] arg){
+}
+
+}
